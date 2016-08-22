0ae5740aaec2ac17d57f48899a2460bc11551b30
WinnyTroy <kiraguwinnie@gmail.com>
2016-08-22 09:32:56 +0300

    files ammended views and app

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:troy@localhost/yi'
 db = SQLAlchemy(app)
-
-# Create our database model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, email):
-        self.email = email
-
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
-
-# Set "homepage" to index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 # Save e-mail to database and send to success page
 @app.route('/prereg', methods=['POST'])
--- views.py
+++ views.py
@@ -1,1 +1,6 @@
 import psycopg2
+
+# Set "homepage" to index.html
+@app.route('/')
+def index():
+    return render_template('index.html')
